b636f73e4372bac0e14bebdf54497160ca4e2ffd
Paulo Rogerio <paulo@intranet>
2024-08-28 11:23:59 -0300

    feat: Autoloading, namespace and models

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Support\Arr;
+use App\Models\Job;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home');
@@ -9,48 +10,16 @@ Route::get('/', function () {
 
 Route::get('/jobs', function () {
     return view('jobs', [
-        'jobs' => [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40.000'
-            ],
-        ]
+        'jobs' => Job::all()
     ]);
 });
 
-
 Route::get('/jobs/{id}', function ($id) {
-    $jobs = [
-         [
-            'id' => 1,
-            'title' => 'Director',
-            'salary' => '$50.000'
-        ],
-        [
-            'id' => 2,
-            'title' => 'Programmer',
-            'salary' => '$10.000'
-        ],
-        [
-            'id' => 3,
-            'title' => 'Teacher',
-            'salary' => '$40.000'
-        ],
-    ];
+    $job = Job::find($id);
 
-    $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-
+    if(!$job) {
+        abort(404);
+    }
     return view('job', ['job' => $job]);
 });
 
