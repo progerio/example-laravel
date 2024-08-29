140ec18d17bb94bbb134754e5cbac694fd9e7e86
Paulo Rogerio <paulo@intranet>
2024-08-29 09:29:28 -0300

    feat: Meet Eloquent

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -2,32 +2,11 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Model;
 
-class Job {
-    public static function all(): array
-    {
-        return  [
-            [
-                'id' => 1,
-                'title' => 'Director',
-                'salary' => '$50.000'
-            ],
-            [
-                'id' => 2,
-                'title' => 'Programmer',
-                'salary' => '$10.000'
-            ],
-            [
-                'id' => 3,
-                'title' => 'Teacher',
-                'salary' => '$40.000'
-            ],
-        ];
-    }
+class Job extends Model{
 
-    public static  function find(int $id): ?Job
-    {
-        return Arr::first(static::all(), fn($job) => $job['id'] == $id);
-    }
+    protected $table = 'jobs_listings';
+
+    protected $fillable = ['title', 'salary'];
 }
